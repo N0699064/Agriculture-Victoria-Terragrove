10fcd6d6e0d513e0f10377a39a4703ff3c404430
Jeff Conrad <32670731+N0699064@users.noreply.github.com>
2025-09-27 16:21:15 +0100

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,16 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": "warn", 
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-img-element": "warn",
+      "prefer-const": "warn",
+      "react-hooks/exhaustive-deps": "warn"
+    }
+  },
+  {
     ignores: [
       "node_modules/**",
       ".next/**",
